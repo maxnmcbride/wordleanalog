8e743036a6d247428e3982596642381b197ab91b
Martin <souzam@sonic.net>
2023-07-26 13:23:03 -0400

    Rendered Row components programmatically

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css"
 import Row from "./components/Row"
 
 function App() {
+  const rows = [1, 2, 3, 4, 5, 6]
+  const renderedRows = rows.map(row => <Row key={row} />)
   
   return (
     <div className="main-container">
       <h2>Main Container</h2>
-      <Row />
+      {renderedRows}
     </div>
   )
 }
