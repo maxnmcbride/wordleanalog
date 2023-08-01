364705b6c8506a21f2ae27a430983882b1e3a9ac
Leah Cardoz <leahcardoz@gmail.com>
2023-07-31 20:22:08 -0400

    set up example grid with basic grid css formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1>Wordle Analog</h1>
+      <div id="grid-container">
+        <div className="grid-row-1">H</div>
+        <div className="grid-row-1">E</div>
+        <div className="grid-row-1">A</div>
+        <div className="grid-row-1">R</div>
+        <div className="grid-row-1">T</div>
+        <div className="grid-row-2">G</div>
+        <div className="grid-row-2">U</div>
+        <div className="grid-row-2">E</div>
+        <div className="grid-row-2">S</div>
+        <div className="grid-row-2">S</div>
+        <div className="grid-row-3">C</div>
+        <div className="grid-row-3">O</div>
+        <div className="grid-row-3">D</div>
+        <div className="grid-row-3">E</div>
+        <div className="grid-row-3">S</div>
+        <div className="grid-row-4">I</div>
+        <div className="grid-row-4">R</div>
+        <div className="grid-row-4">A</div>
+        <div className="grid-row-4">T</div>
+        <div className="grid-row-4">E</div>
+        <div className="grid-row-5">L</div>
+        <div className="grid-row-5">O</div>
+        <div className="grid-row-5">S</div>
+        <div className="grid-row-5">E</div>
+        <div className="grid-row-5">R</div>
+        <div className="grid-row-6">D</div>
+        <div className="grid-row-6">R</div>
+        <div className="grid-row-6">E</div>
+        <div className="grid-row-6">A</div>
+        <div className="grid-row-6">M</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
