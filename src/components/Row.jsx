b20a1361c89c1e766841cbf15cd881ba9b19a877
Martin <souzam@sonic.net>
2023-08-23 16:10:11 -0400

    Fixed prop destructuring issue

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,26 +1,21 @@
 import { useSelector } from "react-redux/es/hooks/useSelector"
 import LetterBox from "./LetterBox"
 
-const Row = (guessIndex) => {
+const Row = ({guessIndex}) => {
     const guesses = useSelector(state => state.guesses)
 
     const tempLetters = ["", "", "", "", ""]
-    let count = 0
 
     let letterBoxes = tempLetters.map((letter, index) => 
         <LetterBox 
             key={index} 
             letter={letter}
-            letterIndex={count}
-        />,
-        count++
+            letterIndex={index}
+        />
     )
 
-    console.log(count)
-
-    if (guesses.hasOwnProperty(guessIndex.guessIndex)) {
-        const guess = guesses[guessIndex.guessIndex].letterVals
-        letterBoxes = guess.map((letter, index) =>         
+    if (guesses.hasOwnProperty(guessIndex)) {
+        letterBoxes = guesses[guessIndex].letterVals.map((letter, index) =>         
             <LetterBox 
                 key={index} 
                 letter={letter}
