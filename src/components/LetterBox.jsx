a7320093f92dd7911fc5c8c61e7c84c1593aa551
Martin <souzam@sonic.net>
2023-08-30 11:58:54 -0400

    Added logic to change LetterBox color based on guesses

--- src/components/LetterBox.jsx
+++ src/components/LetterBox.jsx
@@ -3,8 +3,17 @@ import { useSelector } from "react-redux/es/hooks/useSelector"
 const LetterBox = ({letter, letterIndex}) => {
     const word = useSelector(state => state.words[0].word)
 
+    let bgColor = ""
+
+    if (letter !== "" && word.includes(letter)) {
+        bgColor = "goldenrod" // #B59F3B <-- Official Wordle™ color
+        if (word[letterIndex] === letter) {
+            bgColor = "green"
+        }
+    }
+
     return (
-        <div className="letter-box">
+        <div className="letter-box" style={{backgroundColor: `${bgColor}`}}>
             <p className="display-letter">
                 {letter}
             </p>
